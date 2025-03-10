c5a8d30e177caec489d5c4ebb2248042fd006e0b
CoolShivas <shivas2710cool00@gmail.com>
2025-03-10 09:56:51 +0530

    Starting of Node.js FS Module

---
+++ fsModule/theory.js
@@ -0,0 +1,20 @@
+// /**
+//  * 
+//  * * * // // How to make a folder using Node.js ?
+
+// // With the help of following commands you can make a folder.
+
+// // Step.1) Open the VS Code Terminal.
+
+// // Step.2) Then write the command mkdir fsModule and then hit enter.
+
+// // Step.3) Then the folder is created named as fsModule.
+
+// // Step.4) If you want to move enter then write the command cd fsModule then you can use it.
+//  * 
+//  * 
+//  * 
+//  * 
+//  */
+
+
