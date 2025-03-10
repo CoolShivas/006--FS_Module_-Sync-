6c0b5ece2c72baf3b8ba2a818c7338ab16f17241
CoolShivas <shivas2710cool00@gmail.com>
2025-03-10 11:46:44 +0530

    Resolution to Read data properly in string format.

--- fsModule/sync.js
+++ fsModule/sync.js
@@ -43,7 +43,7 @@ const fs = require("fs");
 
 const fileName = "text.txt";
 const readingFile = fs.readFileSync(fileName);
-console.log(readingFile);
+// console.log(readingFile);
 
 // // // Here, we are getting the output on Terminal as :- 
 // // // Step :-1).    First of all save the program properly.
@@ -52,3 +52,10 @@ console.log(readingFile);
 // // // Step :-4).    Finally, we will get the Output on Terminal as :- 
 // // // <Buffer 54 68 69 73 20 69 73 20 74 68 65 20 66 69 72 73 74 20 66 69 6c 65 20 63 72 65 61 74 65 64 20 62 79 20 75 73 69 6e 67 20 4e 6f 64 65 2e 6a 73 20 77 69 ... 25 more bytes>
 // // // This Buffer occur because we have not mentioned in which format. We want to get the data or content format in which we want.
+
+// // // Therefore, to get the proper format data or content. Use .toString() as mention below.
+console.log(readingFile.toString());
+// // // Now, again repeat the following STEPS OF TERMINAL TO GET THE DATA.
+// // // Finally, we get the Output on the Terminal as :-
+// // // This is the first file created by using Node.js with the help of FS Module.
+// // // Therefore, we are getting the Output in proper string format now.
